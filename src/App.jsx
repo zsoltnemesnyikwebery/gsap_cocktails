2fff31d7a84d4d8b444671197e38078d99d51b81
nemesnyik.zsolt <nemesnyik.zsolt@gb-art.hu>
2025-08-29 16:20:42 +0200

    add About section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText } from "gsap/all"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
+import About from "./components/About"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
